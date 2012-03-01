3648ead7f5b30ae96903d15cbc55be5556031c30
zenna <zennatavares@gmail.com>
2012-02-29 20:58:29 -0500

    initial view

---
+++ server/views.py
@@ -0,0 +1,15 @@
+from django.core.context_processors import csrf
+from django.shortcuts import render_to_response
+from django.http import HttpResponse
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
+from django.db.models import Q
+from django.core.paginator import Paginator, InvalidPage, EmptyPage
+
+from sizeconnect.interface.dashboard.models import Item, Cluster, PairwiseConstraint, Tag
+
+def home(request):
+    """ Generate main content
+        Take filter / order by restraints from POST
+    """
+    return HttpResponse("what is love?")
